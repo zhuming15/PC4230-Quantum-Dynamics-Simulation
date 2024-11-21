644461e20c28b45ac17d9d367aca8991beaf16be
zhuming15 <108252696+zhuming15@users.noreply.github.com>
2024-11-21 18:02:09 +0800

    Add TODO and notes

--- main.m
+++ main.m
@@ -4,21 +4,34 @@ clear;
 clc;
 
 % runSimulation()
-solveEigenvalEigenfunc()
+% solveEigenvalEigenfunc()
 % plotPotentialGraph()
 
+
+% Methodology
+% 1. Find Eigenenergies of Harmonic Oscillator using Hermite Polynomials (Theoretical Value)
+% 2. Find Eigenenergies of Harmonic Oscillator using DVR (Numerical Value)
+% 3. Compure and ensure DVR representation is accurate
+% 4. Prepare the initial state using DVR representation, ensure the parameter is standarized.
+% 5. Implement function to compute transition probability. 
+% 6. Implement function to plot/animate the transition probability.
+% 7. Consider adiabatic limit of omega -> 0, and compare the transition probability with the theoretical value.
+% 8. Consider omega = 0, A -> infinity and compare with mulitple finite square well.
+% 9. Consider omega -> inifnity. State should not be able to reponse to the time dependent potential.
+
 % TODO - Tidy up the plot, include A and omega label in plot
+
 % Define the potential functions
 function plotPotentialGraph
     % Plot parameter
     LEFT_BOUNDARY = -50
     RIGHT_BOUNDARY = 50
     START_TIME = 0
-    END_TIME = 2 * pi
+    END_TIME = 10 * pi
 
     % Potential parameter
-    A = 6
-    OMEGA = 1
+    A = 100
+    OMEGA = 0.5
     
     harmonicPotential = @(x) x.^2 / 2;  % Correct, assumes x is an array
     timeDependentPotential = @(x, A, omega, t) A .* sin(x) .* cos(omega * t);
@@ -40,11 +53,11 @@ function solveEigenvalEigenfunc
 
     % Potential parameter
     A = 0
-    OMEGA = 1
-    TIME = pi / 2
+    OMEGA = 2
+    TIME = 2 *pi
 
     % Plot parameter
-    EIGENFUCNTION_TO_PLOTS = [1,2,3];
+    EIGENFUCNTION_TO_PLOTS = [1, 2, 3, 4, 5];
 
     % Initialize the QuantumDVRDynamicsSolver with DVR parameter
     DVRSolver = QuantumDVRDynamicsSolver(LEFT_BOUNDARY, RIGHT_BOUNDARY, NUM_OF_POINTS);
@@ -69,16 +82,16 @@ function runSimulation
     LEFT_BOUNDARY = -50
     RIGHT_BOUNDARY = 50
     NUM_OF_POINTS = 1000
-    TOT_TIME = 2 * pi
-    NUM_OF_STEPS = 1000
+    TOT_TIME = 10 * pi
+    NUM_OF_STEPS = 10000
 
     %initial state parameter of Guassian Wavapacket
-    X_0 = 10
-    SIGMA = 2
+    X_0 = 0
+    SIGMA = 1
 
     % potential parameter
-    A = 0
-    OMEGA = 1
+    A = 1000
+    OMEGA = 0.5
     
     sim = QuantumDynamicsSimulator(LEFT_BOUNDARY, RIGHT_BOUNDARY, NUM_OF_POINTS, TOT_TIME, NUM_OF_STEPS);
     sim = sim.initializeWavePacket(X_0, SIGMA)
